05ae89d6b0e11926f2d845ec9c8f0aea3dbedf06
Adeel <abhaloo@hotmail.com>
2020-09-01 11:55:22 +0300

    added api routes for user,profiles,auth,posts and created user model

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+//create express instance
+const app = express();
+
+//Connect database
+connectDB();
+
+//this will be invoked when a request is made
+// res - response
+// req - request  
+app.get('/',(req,res) => 
+    res.send('My first API is Running'));
+
+//define routes
+app.use('/api/users',require('./routes/api/users'));
+app.use('/api/auth',require('./routes/api/auth'));
+app.use('/api/profile',require('./routes/api/profile'));
+app.use('/api/posts',require('./routes/api/posts'));
+
+
+const PORT = process.env.PORT || 5000;
+
+//start server
+app.listen(PORT,() => console.log(`Server started on port ${PORT}`));
